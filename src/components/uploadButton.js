fe66adc8886e8d1b9be753fe7031c34ddab978d8
Shashank <geekyholic2275@gmail.com>
2020-10-13 11:49:06 +0530

    add sumbit button appers on image selected

--- src/components/uploadButton.js
+++ src/components/uploadButton.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import SubmitButton from "./SubmitButton";
+import "./upload.css";
 
 function UploadButton() {
-  return <input type="File" />;
+  const [imageSelected, setImage] = useState("");
+
+  return (
+    <>
+      <input type="File" name="image" onChange={(e) => setImage(imageSelected + `${e.target.value}`)} />
+      {imageSelected !== "" ? <SubmitButton /> : <p>no image is selected</p>}
+    </>
+  );
 }
 
 export default UploadButton;
