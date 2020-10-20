9e8e9f9bef39e3aec6c837c6e90ab880a0871e3d
mamoanwar97 <mamoanwar97@gmail.com>
2020-10-20 15:40:43 +0200

    changed the link from plain text to clickable link

--- client/src/components/link.js
+++ client/src/components/link.js
@@ -5,11 +5,9 @@ function Link({URL}) {
   return (
     <div>
       <h1>your link is here</h1>
-      <p>
-        {
-          URL || "wait while link generates"
-        }
-      </p>
+      {
+        URL ? <a href={URL}>{URL}</a> || "wait while link generates"
+      }
       {URL !== null ? <img src={URL} alt="uploaded image" style={{maxWidth: "200px", maxHeight:"200px", objectFit: "contain"}}/>  : null}
     </div>
   );
